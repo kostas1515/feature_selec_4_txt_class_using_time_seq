513f9f7fe7645c652057eff873e186f56409802b
Alexandridis Konstantinos Panagiotis <alexandridis.konpan@hotmail.com>
2019-02-14 13:17:46 +0200

    perform ks to rel_feats separately

---
+++ ks_test.py
@@ -0,0 +1,117 @@
+import os
+from FeatureSelection import FeatureSelection
+import pandas as pd
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix,precision_score,recall_score
+from sklearn.svm import SVC, LinearSVC
+from sklearn import svm
+from sklearn.feature_extraction.text import  TfidfVectorizer,TfidfTransformer,CountVectorizer
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
+import math
+
+# row['percent_train']>0.02
+#category_matrix=["C172","C18","C181","C313","GCRIM","GDIS","GFAS","GJOB","GSCI","GVOTE","GWEA","C183","C22","C32","C411","E13","E212","C173"]
+category_matrix=["C4","C41","C411","C42","C","E1","E11","E12","E121","E13",
+"E131","E132","E14","E141","E142","E143","E2","E21","E211","E212","E3","E31","E311","E312","E313","E4","E41","E411","E5","E51","E511",
+"E512","E513","E6","E61","E7","E71","E","G1","G15","G151","G152","G153","G154","G155","G156","G157","G158","G159","G","GCRIM","GDEF","GDIP","GDIS","GENT","GENV","GFAS","GHEA","GJOB","GOBIT","GODD","GPOL","GPRO","GREL","GSCI","GSPO","GTOUR",
+"GVIO","GVOTE","GWEA","GWELF","M1","M11","M12","M13","M131","M132","M14","M141","M142","M143","M"]
+
+for category in category_matrix:
+    df=pd.DataFrame()
+    f1=pd.DataFrame()
+
+    bench=FeatureSelection(category,389827) #enter target category and the last id of the preffered train_set
+    #use relative path
+    for csv in os.listdir("../testspace2/csvs"):
+        data = pd.read_csv("../testspace2/csvs/"+csv, encoding = 'iso-8859-1')
+        bench.split_data(data)
+
+    label_train=bench.y_train
+    vectorizer = CountVectorizer(lowercase=False)
+    x_train = vectorizer.fit_transform(bench.x_train)
+    x_test=vectorizer.transform(bench.x_test)
+    
+    label_test=bench.y_test
+    
+    x_rel_train=bench.get_x_rel_train(x_train,label_train)
+    
+    x_train,x_test=bench.remove_non_rel_features(x_train,x_test)
+    
+    threshold=[95,90,85,80,70,50,20,10,5]
+    
+    
+    feature_amount=x_train.shape[1]
+    
+    uni_order_score_ks=bench.quick_uniform(x_rel_train)
+
+    uni_stamp_score_ks=bench.quick_uniform2(x_rel_train)
+
+    
+
+
+
+    ###################################### Uniform time order ###############################
+    p_p=[]
+    r_r=[]
+    fm=[]
+
+    for thresh in threshold:
+        k=math.floor(feature_amount*thresh/100)
+        new_x_train,new_x_test=bench.transform_features(x_train,x_test,uni_order_score_ks,k)
+
+            #tfidf
+        t_vectorizer = TfidfTransformer()
+        x_train_init = t_vectorizer.fit_transform(new_x_train)
+        x_test_init= t_vectorizer.transform(new_x_test)
+        
+        #classification
+
+        clf = svm.LinearSVC(random_state=1).fit(x_train_init, label_train)
+        test_test_predict = clf.predict(x_test_init)
+
+
+        p_p.append(precision_score(label_test, test_test_predict))
+        r_r.append(recall_score(label_test, test_test_predict))
+        fm.append(f1_score(label_test, test_test_predict))
+        
+
+    df["uniformO_p_ks"]=p_p
+    df["uniformO_r_ks"]=r_r
+    f1["uniformO_f1_ks"]=fm
+
+
+    ############################### UNIFORM TIME STAMP #################################
+
+    p_p=[]
+    r_r=[]
+    fm=[]
+
+    for thresh in threshold:
+        k=math.floor(feature_amount*thresh/100)
+        new_x_train,new_x_test=bench.transform_features(x_train,x_test,uni_stamp_score_ks,k)
+
+            #tfidf
+        t_vectorizer = TfidfTransformer()
+        x_train_init = t_vectorizer.fit_transform(new_x_train)
+        x_test_init= t_vectorizer.transform(new_x_test)
+        
+        #classification
+
+        clf = svm.LinearSVC(random_state=1).fit(x_train_init, label_train)
+        test_test_predict = clf.predict(x_test_init)
+
+
+        p_p.append(precision_score(label_test, test_test_predict))
+        r_r.append(recall_score(label_test, test_test_predict))
+        fm.append(f1_score(label_test, test_test_predict))
+        
+
+    df["uniformS_p_ks"]=p_p
+    df["uniformS_r_ks"]=r_r
+    f1["uniformS_f1_ks"]= fm
+    
+
+
+
+    df.to_csv('csvs_revised/ks/' + category+'.csv', index=False)
+    f1.to_csv('csvs_revised/ks/' + category+'.csv', index=False)
